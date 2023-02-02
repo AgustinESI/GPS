d9c87bd7d49f06dbeb515f2c0919da77d65e8314
AgustinESI <Agustin.Sanchez9@alu.uclm.es>
2023-02-02 02:01:28 +0100

    Created layout

--- app/src/main/java/com/GSI/SanchezVillafranca/gps/MainActivity.java
+++ app/src/main/java/com/GSI/SanchezVillafranca/gps/MainActivity.java
@@ -1,14 +1,37 @@
 package com.GSI.SanchezVillafranca.gps;
 
+import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.location.FusedLocationProviderClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updates, tv_address;
+    Switch sw_locationsupdates, sw_gps;
+
+    // API de google para el cliente
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_lat = findViewById(R.id.tv_lat);
+        tv_lon = findViewById(R.id.tv_lon);
+        tv_altitude = findViewById(R.id.tv_altitude);
+        tv_accuracy = findViewById(R.id.tv_accuracy);
+        tv_speed = findViewById(R.id.tv_speed);
+        tv_sensor = findViewById(R.id.tv_sensor);
+        tv_updates = findViewById(R.id.tv_updates);
+        tv_address = findViewById(R.id.tv_address);
+        sw_locationsupdates = findViewById(R.id.sw_locationsupdates);
+        sw_gps = findViewById(R.id.sw_gps);
+
+
     }
 }
